1c747d0a5e69b67b6afad266fe9c42384ae4e637
sanaz.javadi <sanaz.javadi@zoodfood.com>
2022-06-12 19:59:29 +0430

    initial sass loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import './styles.css'
+import './styles.scss'
 
 export const App = () => {
   return (
-    <h1>
-      {process.env.name} -React TypeScript Webpack Starter Template -{' '}
-      {process.env.NODE_ENV}
-    </h1>
+    <div className="app">
+      <h1>{process.env.name} -React TypeScript Webpack Starter Template</h1>
+      <h2>mode : {process.env.NODE_ENV}</h2>
+      <div>
+        <h3>svg</h3>
+        <img
+          src={'/public/assets/icons/happy.svg'}
+          alt={'happy'}
+          height={50}
+          width={50}
+        />
+      </div>
+      <div>
+        <h3>png</h3>
+        <img
+          src={'/public/assets/icons/logo192.png'}
+          alt={'logo'}
+          height={50}
+          width={50}
+        />
+      </div>
+      <div>
+        <h3>font</h3>
+        <p>ایران یکان</p>
+      </div>
+    </div>
   )
 }
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,14 +1,18 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
+  devServer: {
+    historyApiFallback: true, // fix 404 error on reloads when using router
+    port: 7000, // runs the application on port 7000 in dev mode
+  },
   module: {
-    rules:      
-        [
+    rules: [
       {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
@@ -30,15 +34,34 @@ module.exports = {
         test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
         type: 'asset/inline',
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
     ],
   },
   output: {
     path: path.resolve(__dirname, '..', './build'),
-    filename: 'bundle.js',
+    filename: '[name].[hash].bundle.js',
+    clean: true, // cleans build folder before execution of new build process
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './src/index.html'),
+    }),
+    new CopyPlugin({
+      // copies public folder to build folder
+      patterns: [{ from: './src/public', to: './public' }],
+      options: {
+        concurrency: 100,
+      },
     }),
   ],
 }
